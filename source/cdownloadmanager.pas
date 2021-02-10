6f02b607116a32e61ed414cbfab7c1f66d20ab1e
wp_xyz <wp_xxyyzzz@gmx.net>
2021-02-10 09:36:56 +0100

    Fix new downloader not writing files. Update of data files.

--- source/cdownloadmanager.pas
+++ source/cdownloadmanager.pas
@@ -1,25 +1,5 @@
-{
-  MIT License
-
-  Copyright (c) 2021 Gustavo Carreno <guscarreno@gmail.com>
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to
-  deal in the Software without restriction, including without limitation the
-  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-  sell copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in
-  all copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-  IN THE SOFTWARE.
+{ Code kindly provided by
+    (c) 2021 Gustavo Carreno <guscarreno@gmail.com>
 
   Minor changes by Werner Pamler
 }
@@ -45,7 +25,7 @@ type
     FURL: String;
     FFilename: String;
     FOnShowStatus: TShowStatusEvent;
-    procedure DataReceived(Sender : TObject; const ContentLength, CurrentPos : Int64);
+    procedure DataReceived(Sender : TObject; const {%H-}ContentLength, CurrentPos : Int64);
     procedure ShowStatus;
   protected
     procedure Execute; override;
@@ -127,6 +107,7 @@ var
   http: TFPHTTPClient;
   headers: TStringList;
   index: Integer;
+  ms: TMemoryStream;
 begin
   http:= TFPHTTPClient.Create(nil);
   http.AllowRedirect:= True;
@@ -144,7 +125,13 @@ begin
         end;
       end;
       http.OnDataReceived:= @DataReceived;
-      http.Get(FURL);
+      ms := TMemoryStream.Create;
+      try
+        http.Get(FURL, ms);
+        ms.SaveToFile(FFileName);
+      finally
+        ms.Free;
+      end;
     except
       on E: Exception do
       begin
