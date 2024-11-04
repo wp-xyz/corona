10fc5967c8319a475f789f6893c271c5ccbf5659
Artem V. Ageev <artem.v.ageev@gmail.com>
2024-11-04 22:40:50 +0200

    fix make.sh

--- make.sh
+++ make.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+##############################################################################################################
 
 function priv_clippit
 (
@@ -9,64 +10,49 @@ Options:
 EOF
 )
 
-function priv_packages
+function priv_dpkg
 (
-    if [[ -d 'use' ]]; then
-        git submodule update --init --recursive
-        git submodule update --recursive --remote
-    else
-        mkdir 'use'
-    fi
-    if ((${#})); then
-        for REPLY in "${@}"; do
-            declare -A VAR=(
-                [url]="https://packages.lazarus-ide.org/${REPLY}.zip"
-                [out]=$(mktemp)
-            )
-            wget --output-document "${VAR[out]}" "${VAR[url]}"
-            unzip -o "${VAR[out]}" -d "use/${REPLY}"
-            rm --verbose "${VAR[out]}"
-        done
-    fi
-    find 'use' -type 'f' -name '*.lpk' -exec lazbuild --add-package-link {} +
-)
-
-function priv_pkgsearch
-(
-    if ((${#})); then
-        for REPLY in "${@}"; do
-            lazbuild --verbose-pkgsearch "${REPLY}" || lazbuild --add-package "${REPLY}"
-        done
-    fi
+    # Linux shell script to create .deb package
+    # Go to the repository local folder. For example, cd /home/User/ShellRemoteBot
+    declare -r STAGING_DIR=./deb.staging
+    find "${STAGING_DIR}" -type d -exec chmod 0755 {} +
+    find "${STAGING_DIR}" -type f -exec chmod 0644 {} +
+    find "${STAGING_DIR}/usr/bin" -type f -exec chmod 0755 {} +
+    echo "Installed-Size: $(du -s ${STAGING_DIR} | awk '{print $1;}')" >>"${STAGING_DIR}/DEBIAN/control"
+    gzip -9 -n 'deb.staging/usr/share/doc/tgshd/changelog.Debian'
+    read -rp "Input package filename: "
+    dpkg-deb --root-owner-group --build "${STAGING_DIR}" "${REPLY}.deb"
 )
 
 function priv_main
 (
     set -euo pipefail
-    if ! (which lazbuild); then
-        source '/etc/os-release'
-        case ${ID:?} in
-            debian | ubuntu)
-                sudo apt-get update
-                sudo apt-get install -y lazarus shellcheck shfmt
-                ;;
-        esac
-    fi
-    shellcheck --external-sources "${0}" 1>&2
-    shfmt -ci -fn -i 4 -d "${0}" 1>&2
     if ((${#})); then
         case ${1} in
             build)
-                declare -ra PKG LNK=('PlaysoundPackage' 'Rx' 'FPSpreadsheet')
-                priv_pkgsearch "${PKG[@]}"
-                priv_packages "${LNK[@]}"
-                find 'src' -type 'f' -name '*.lpi' \
-                    -exec lazbuild --no-write-project --recursive --no-write-project --build-mode=release {} + 1>&2
+                if ! (which 'lazbuild'); then
+                    source '/etc/os-release'
+                    case ${ID:?} in
+                        debian | ubuntu)
+                            sudo apt-get update
+                            sudo apt-get install -y lazarus
+                            ;;
+                    esac
+                fi
+                if [[ -d 'use' ]]; then
+                    git submodule update --recursive --init
+                    git submodule update --recursive --remote
+                    find 'use' -type 'f' -name '*.lpk' -exec lazbuild --add-package-link {} +
+                fi
+                find 'src' -type 'f' -name '*.lpi' -exec \
+                    lazbuild --no-write-project --recursive --no-write-project --build-mode=release {} + 1>&2
                 ;;
+            dpkg) priv_dpkg ;;
         esac
     else
         priv_clippit
     fi
 )
 
+##############################################################################################################
 priv_main "${@}" >/dev/null
